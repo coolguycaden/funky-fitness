51a3c2132a0ab99ea220b64a4fd328dd384a768c
chugjugloli <cadenallen97@gmail.com>
2024-06-26 17:45:28 -0400

    fixed-anew

--- lib/main.dart
+++ lib/main.dart
@@ -24,9 +24,11 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: const BaseApp(),
       routes: <String, WidgetBuilder> {
+
         '/WorkoutScreen' : (context) => const WorkoutScreen(),
         '/StatScreen' : (context) => const StatScreen(),
         '/ProfileScreen' : (context) => const ProfileScreen(),
+        
       }
     );
   }
@@ -39,57 +41,44 @@ class BaseApp extends StatelessWidget {
 
     //Used as root of application, configure themes / routes
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
 
-          
-
-
-          backgroundColor: Colors.blue,
-          title: const Text('Rizzy bear'),
-        ),
-
-      body: Container(
-          
-          margin: const EdgeInsets.all(100),
-          padding: const EdgeInsets.all(10),
-          color: Colors.red,
-          height: 100,
-          width: 100,
-
-          /*Extra customization options
-          decoration: const BoxDecoration(
-
-          ),*/
-
-          child: const Text('Floating'),
-        ),
         
-        bottomNavigationBar: BottomNavigationBar(
-              
 
-              items: const [
-          
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.home),
-                  label: 'Workouts',
-                  
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.line_weight),
-                  label: 'Statistics',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.masks),
-                  label: 'Profile',
-                ),
 
-              ],
-              onTap: (value) {
-                if (value == 0) Navigator.pushNamed(context, '/WorkoutScreen');
-                if (value == 1) Navigator.pushNamed(context, '/StatScreen');
-                if (value == 2) Navigator.pushNamed(context, '/ProfileScreen');
-              },
-        )
+        backgroundColor: Colors.blue,
+        title: const Text('Rizzy bear'),
+      ),
+        
+      bottomNavigationBar: BottomNavigationBar(
+            
+        items: const [
+    
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Workouts',
+            
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.line_weight),
+            label: 'Statistics',
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.masks),
+            label: 'Profile',
+          ),
+
+        ],
+
+        onTap: (value) {
+          if (value == 0) Navigator.pushNamed(context, '/WorkoutScreen');
+          if (value == 1) Navigator.pushNamed(context, '/StatScreen');
+          if (value == 2) Navigator.pushNamed(context, '/ProfileScreen');
+        },
+
+      )
          
         
       );

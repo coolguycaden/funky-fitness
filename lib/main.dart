3c0bd526ea13db1e882644f7103a16fe64317ae1
chugjugloli <cadenallen97@gmail.com>
2024-07-04 22:36:09 -0400

    new start

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 // List<Widget> pageList = [
 //   const WorkoutScreen(),
 //   const StatScreen(),
@@ -22,7 +21,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const BaseApp(),
+      home: BaseApp(),
       routes: <String, WidgetBuilder> {
 
         '/WorkoutScreen' : (context) => const WorkoutScreen(),
@@ -33,65 +32,39 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
-class BaseApp extends StatelessWidget {
+class BaseApp extends StatelessWidget {//with SingleTickerProviderStateMixin {
   const BaseApp({super.key});
 
+  //int _currentIndex = 0;
+  /*late TabController tabController;
+  @override
+
+  void initState(){
+    tabController = TabController(length: 3, vsync: this);
+    tabController.addListener(() {
+      setState(() {
+        
+      });
+    });
+
+    super.initState();
+  }
+*/
   @override
   Widget build(BuildContext context) {
 
-    //Used as root of application, configure themes / routes
-    /*return Scaffold(
-      appBar: AppBar(
-
-        
-
-
-        backgroundColor: Colors.blue,
-        title: const Text('Rizzy bear'),
-      ),
-        
-      bottomNavigationBar: BottomNavigationBar(
-            
-        items: const [
-    
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Workouts',
-            
-          ),
-
-          BottomNavigationBarItem(
-            icon: Icon(Icons.line_weight),
-            label: 'Statistics',
-          ),
-
-          BottomNavigationBarItem(
-            icon: Icon(Icons.masks),
-            label: 'Profile',
-          ),
-
-        ],
-
-        onTap: (value) {
-          if (value == 0) Navigator.pushNamed(context, '/WorkoutScreen');
-          if (value == 1) Navigator.pushNamed(context, '/StatScreen');
-          if (value == 2) Navigator.pushNamed(context, '/ProfileScreen');
-        },
-
-      )
-         
-        
-      );*/
-
-    return const MaterialApp(
+    return MaterialApp(
       home: DefaultTabController(
         length: 3,
+        
         child: Scaffold(
           bottomNavigationBar: TabBar(
-            tabs: [
+          
+          //c: _currentIndex,
+            tabs: const [
                 Tab(
                   text: 'Workouts',
-                  icon: Icon(Icons.home),
+                  icon: Icon(Icons.fitness_center),
                 ),
                 
                 Tab(
@@ -102,45 +75,24 @@ class BaseApp extends StatelessWidget {
                 Tab(
                   text: 'Profile',
                   icon: Icon(Icons.face),
+                  
                 ),
             ],
+
+  
+            /*onTap: (value) {
+              if (value == 0) Navigator.pushNamed(context, '/WorkoutScreen');
+              if (value == 1) Navigator.pushNamed(context, '/StatScreen');
+              if (value == 2) Navigator.pushNamed(context, '/ProfileScreen');
+        
+            },*/
             
           ),
+          
         ),
+
       ),
-      /*home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: const TabBar(
-              tabs: [
-                Tab(
-                  icon: Icon(Icons.home),
-                ),
-                
-                Tab(
-                  icon: Icon(Icons.bar_chart),
-                ),
-                
-                Tab(
-                  icon: Icon(Icons.face),
-                ),
-              ],
-            ),
-
-            
-          ),
-
-          body: const TabBarView(
-            children: [
-              Icon(Icons.home),
-              Icon(Icons.bar_chart),
-              Icon(Icons.face),
-            ],
-          ),
-
-        ),
-      ),*/
+    
     );
   
   }
@@ -173,7 +125,7 @@ class StatScreen extends StatelessWidget {
 
           backgroundColor: Colors.green,
           title: const Text('Welcome to Stats!'),
-        ),
+      ),
 
     );
   }
